79b5901fbadf1214d80edb85483319a81e37cd31
skyper-cz <62872636+skyper-cz@users.noreply.github.com>
2022-10-05 12:20:40 +0200

    Otazky

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import javax.swing.*;
+
 public class Main {
+
+    public static JFrame fr = new JFrame("Quiz");
+    public static Otazka[] otazky = new Otazka[10];
+    public static String[] otazicky = new String[]{"Kdo koupil niHao?", "Kdo nás opustil v roce 2021?",
+            "Která značka chytrých mobilních telefonů jako první představila sateletní komunikaci?",
+            "Jak se jmenuje vozídko podle něhož Láďa a Zdeněk staví své?",
+            "Které letectvo sundavalo led z letadel pomocí kladiv?",
+            "Odkud je televize naší třídy?", "Jakou barvu vlasů měl Vítek na začátku Druháku?",
+            "Jakého tanku bylo vyrobeno nejvíce?", "Kolik Linux zařízení máme ve třídě?",
+            "Jaký je nejlepší tank AČR?"};
+    public static String[] odpovede1 = new String[]{"Karel", "Barrack Obama", "Huawei", "Josif", "USAF", "Ze Znojma", "Růžovou", "T-72", "1", "T-72 Scarab"};
+    public static String[] odpovede2 = new String[]{"Vítek", "Karel Gott", "Apple", "Zeman", "SSRAF", "Z Číny", "Modrou", "T-34", "3", "T-72M4CZ"};
+    public static String[] odpovede3 = new String[]{"Mikuláš", "Gorbačov", "Samsung", "Miloš", "RAF", "Z Japonska", "Červenou", "T-54/55", "4", "T-72M5"};
+    public static String[] odpovede4 = new String[]{"Mikoláš", "Fabián", "Motorola", "Rommel", "ČSLL", "Z Polska", "Oranžovou", "M4 Sherman", "5", "T-90"};
+    public static String[] spravnaOdpoved = new String[]{"Vítek", "Fabián", "Huawei", "Miloš", "SSRAF", "Z Polska", "Červenou", "T-54/55", "5", "T-72M4CZ"};
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        for (int i = 0; i < otazky.length; i++){
+            Otazka vlozit = new Otazka();
+            vlozit.setQuestion(otazicky[i]);
+            vlozit.setMoznost1(odpovede1[i]);
+            vlozit.setMoznost2(odpovede2[i]);
+            vlozit.setMoznost3(odpovede3[i]);
+            vlozit.setMoznost4(odpovede4[i]);
+            vlozit.setSpravnaOdpoved(spravnaOdpoved[i]);
+            otazky[i] = vlozit;
+        }
+        
     }
 }
---
+++ src/Otazka.java
@@ -0,0 +1,57 @@
+public class Otazka {
+    String question;
+    String moznost1;
+    String moznost2;
+    String moznost3;
+    String moznost4;
+    String spravnaOdpoved;
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getMoznost1() {
+        return moznost1;
+    }
+
+    public void setMoznost1(String moznost1) {
+        this.moznost1 = moznost1;
+    }
+
+    public String getMoznost2() {
+        return moznost2;
+    }
+
+    public void setMoznost2(String moznost2) {
+        this.moznost2 = moznost2;
+    }
+
+    public String getMoznost3() {
+        return moznost3;
+    }
+
+    public void setMoznost3(String moznost3) {
+        this.moznost3 = moznost3;
+    }
+
+    public String getMoznost4() {
+        return moznost4;
+    }
+
+    public void setMoznost4(String moznost4) {
+        this.moznost4 = moznost4;
+    }
+
+    public String getSpravnaOdpoved() {
+        return spravnaOdpoved;
+    }
+
+    public void setSpravnaOdpoved(String spravnaOdpoved) {
+        this.spravnaOdpoved = spravnaOdpoved;
+    }
+}
+
